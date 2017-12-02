2fb98e663551a499a357156206f8bdbf46ed2617
EliseyMuray <muray.apple@icloud.com>
2017-12-02 21:27:01 +0200

    1.10

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,9 +6,7 @@ function guess() {
 //add functionality to guess function here
 }
 function setHiddenFields () {
-  let answer = function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+  answer.value = Math.floor(Math.random() * 9999);
   while (answer.lenght < 4) {
   "0" + toString(answer)
   }
