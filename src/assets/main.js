aabfe55e9f4f77b925e803a3f94e5486c90408aa
EliseyMuray <muray.apple@icloud.com>
2017-12-02 21:18:04 +0200

    1.8

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,10 +5,11 @@ function guess() {
     let input = document.getElementById('user-guess');
 //add functionality to guess function here
 }
-function setHiddenFields (min, max) {
-  var answer = Math.floor(Math.random() * (max - min + 1)) + min;
+function setHiddenFields (random) {
+  random = Math.random();
+  var answer = Math.floor(random*9999);
   while (answer.lenght < 4) {
-
+  "0" + toString(answer)
   }
 return answer;
 }
