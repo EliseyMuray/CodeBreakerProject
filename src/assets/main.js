03f621edd21d3ba370417f7c3efc017960b21107
EliseyMuray <muray.apple@icloud.com>
2017-12-02 21:01:04 +0200

    1.6

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,10 +5,11 @@ function guess() {
     let input = document.getElementById('user-guess');
 //add functionality to guess function here
 }
-function setHiddenFields () {
-  var answer = Math.floor(Math.random() * 9999);
+function setHiddenFields (A) {
+  A = Math.random() * 9999;
+  var answer = Math.floor(A);
   while (answer.lenght < 4) {
-        
+
   }
 return answer;
 }
