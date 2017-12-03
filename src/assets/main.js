7109d4605b40795d6f173f8f0b4c346ec28cdefa
EliseyMuray <muray.apple@icloud.com>
2017-12-03 14:11:27 +0200

    5.3

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,7 +11,7 @@ function guess() {
 
 function setHiddenFields () {
   answer.value = Math.floor(Math.random() * 9999).toString();
-  attempt.value = "0";
+  attempt.value = "0"; 
   while (answer.lenght < 4) {
   answer.value = "0" + answer.value;
   }
