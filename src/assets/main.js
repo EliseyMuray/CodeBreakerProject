1df4119db5ad93f199ed14262b1d7c9a2ec454de
EliseyMuray <muray.apple@icloud.com>
2017-12-03 21:25:17 +0200

    1

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,23 +6,28 @@ function guess() {
      if(answer.value == "" || attempt.value == "") {
        setHiddenFields ();
      }
-
+     if (validateInput(input.value) == false) {
+       return false;
+     } else {
+       attempt.value ++;
+     }
 //add functionality to guess function here
 }
 
 function setHiddenFields () {
   answer.value = Math.floor(Math.random() * 9999).toString();
-  attempt.value = "0";
-  while (answer.lenght < 4) {
+
+  while (answer.value.lenght < 4) {
   answer.value = "0" + answer.value;
   }
+  attempt.value = 0;
 }
 
 function setMessage (message) {
   document.getElementById('message').innerHTML=message;
 }
 
-function validateInput (input.value) {
+function validateInput (input) {
   if(input.lenght != 4) {
     setMessage ("Guesses must be exactly 4 characters long.");
     return false;
