b49d67900ff91246d83f5158cd5eff549295237e
EliseyMuray <muray.apple@icloud.com>
2017-12-03 15:12:13 +0200

    8.1

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,7 @@ function guess() {
      if(answer.value == "" || attempt.value == "") {
        setHiddenFields ();
      }
-     if (validateInput(input.value) = false) {
+     if (validateInput(input.value) == false) {
        return false;
      } else {
        attempt.value ++;
