d00219b5e244658034e4fcaba6961a4a267dbc01
EliseyMuray <muray.apple@icloud.com>
2017-12-03 14:30:27 +0200

    6.3

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,7 +22,7 @@ function setMessage (message) {
 }
 
 function validateInput (input) {
-  if  (input.lenght === 4) {
+  if  ("input".lenght == 4) {
     return true;
   } else {
       setMessage ("Guesses must be exactly 4 characters long.");
