eff26e7726653aacba4520aaf683713d3f6c7130
EliseyMuray <muray.apple@icloud.com>
2017-12-05 20:05:55 +0200

    111

--- src/assets/main.js
+++ src/assets/main.js
@@ -13,32 +13,31 @@ function guess() {
      }
 //add functionality to guess function here
 }
+
 function getResults(input){
-    let correct = 0;
-    let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
-    for(i = 0; i < input.length; i++)
-    {
-        if(input.charAt(i) == answer.value.charAt(i))
-        {
-            html += '<span class="glyphicon glyphicon-ok"></span>';
-            correct++;
-        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
-            html += '<span class="glyphicon glyphicon-transfer"></span>';
-        } else {
-            html += '<span class="glyphicon glyphicon-remove"></span>';
-        }
-    }
-    html += '</div></div>';
+  var correct = 0;
+  var html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for (i = 0; i < input.lenght; i++)
+  {
+    if(input.charAt(i) == answer.value.charAt(i))
+      {
+           html += `<span class="glyphicon glyphicon-ok"></span>`;
+           correct++;
+     } else if (answer.value.indexOf(input.charAt(i))>-1) {
+           html += `<span class="glyphicon glyphicon-transfer"></span>`;
+     } else {
+           html += `<span class="glyphicon glyphicon-remove"></span>`;
+     }
+  }
+  html += "</div></div>";
+  document.getElementById('results').innerHTML += html;
 
-    document.getElementById('results').innerHTML += html;
-
-    if(correct == input.length) {
+  if(correct == input.lenght) {
         return true;
-    } else {
+  } else {
         return false;
-    }
+  }
 }
-
 
 
 
