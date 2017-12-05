c831c98121b65c1165cd866b27990ec9f9552f85
EliseyMuray <muray.apple@icloud.com>
2017-12-05 19:12:56 +0200

    !2

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,6 +14,20 @@ function guess() {
 //add functionality to guess function here
 }
 
+function getResults (input.value) {
+  var result = "`<div class="row"><span class="col-md-6">'" + input + "'</span><div class="col-md-6">`";
+  for (i=0; input.lenght<4; i++) {
+    if (input.value.charAt(i) == answer.value.charAt(i)) {
+      result = result + `<span class="glyphicon glyphicon-ok"></span>`;
+    } else if (input.value.charAt(i) == answer.value.charAt(0) || input.value.charAt(i) == answer.value.charAt(1) || input.value.charAt(i) == answer.value.charAt(2) || input.value.charAt(i) == answer.value.charAt(3)) {
+      result = result + `<span class="glyphicon glyphicon-transfer"></span>`;
+    } else {
+      result = result + `<span class="glyphicon glyphicon-remove"></span>`;
+    }
+  }
+  document.getElementById('results').innerHTML=result;
+}
+
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 10000).toString();
     while(answer.value.length < 4) {
@@ -34,18 +48,6 @@ function validateInput(input) {
     return true;
 }
 
-function getResults (input.value) {
-  var result = "`<div class="row"><span class="col-md-6">'" + input + "'</span><div class="col-md-6">`";
-  for (i=0; input.lenght<4; i++) {
-    if (input.charAt(i) == answer.value.charAt(i)) {
-      result = result + `<span class="glyphicon glyphicon-ok"></span>`;
-    } else if (input.charAt(i) == answer.value.charAt(0) || input.charAt(i) == answer.value.charAt(1) || input.charAt(i) == answer.value.charAt(2) || input.charAt(i) == answer.value.charAt(3)) {
-      result = result + `<span class="glyphicon glyphicon-transfer"></span>`;
-    } else {
-      result = result + `<span class="glyphicon glyphicon-remove"></span>`;
-    }
-  }
-  document.getElementById('results').innerHTML=result;
-}
+
 
 //implement new functions here
